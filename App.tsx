7e523854b9d0cbfc04fa93335b2159a16c8bf0ec
Aditya Kumar <aadikori9085@gmail.com>
2025-06-15 03:39:50 +0530

    Update the App.js

--- App.tsx
+++ App.tsx
@@ -1,12 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -15,6 +9,7 @@ function App() {
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <NewAppScreen templateFileName="App.tsx" />
+      <Text>Here are the update</Text>
     </View>
   );
 }
